0a273caca1f0f9a18a0d0c06556d29f095b5a55c
pratoshkumarn <116501465+pratoshkumarn@users.noreply.github.com>
2025-03-30 15:57:14 +0530

    Update App.jsx

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { BrowserRouter as Router, Routes, Route, Link , Navigate} from "react-router-dom";
 import Home from "./home";
 import Hire from "./Hire";
 import Contact from "./Contact";
